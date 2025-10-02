585e6eddeec0db2a0d4f944a54752452cfa9e1cc
luana.silva <luana.silva@bitgov.com.br>
2025-10-01 21:34:15 -0300

    Aula 05 - refatoração do layout com bootstrap e implementação do async/await.

--- Front-End Dinamico (JS DOM)/Loja-Online/script.js
+++ Front-End Dinamico (JS DOM)/Loja-Online/script.js
@@ -3,12 +3,13 @@ const catalogoProduto = document.querySelector('#catalogo');
 const formProduto = document.getElementById('form-produto');
 const inputTitulo = document.getElementById('input-titulo');
 const inputPreco = document.getElementById('input-preco');
+const destaqueProduto = document.getElementById('destaque');
 
 let listaDeProdutos = [];
 
 function criarCardProduto(produto) {
     const card = document.createElement('article');
-    card.className = 'card-produto';
+    card.className = 'card-produto col-sm-12 col-md-6 col-lg-3';
 
     const imagem = document.createElement('img');
     imagem.src = produto.image || 'https://picsum.photos/200/300';
@@ -39,15 +40,60 @@ function salvarProdutosNoStorage() {
     localStorage.setItem('produtos', JSON.stringify(listaDeProdutos));
 }
 
-function buscarProdutosDaApi() {
-    fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
-        .then(produtos => {
-            listaDeProdutos = produtos;
-            renderizarProdutos();
-            salvarProdutosNoStorage();
-        })
-        .catch(erro => console.error('Erro ao buscar os produtos:', erro));
+// function buscarProdutosDaApi() {
+//     fetch('https://fakestoreapi.com/products')
+//     .then(response => response.json())
+//     .then(produtos => {
+//         listaDeProdutos = produtos;
+//         renderizarProdutos();
+//         salvarProdutosNoStorage();
+
+//         const primeiroProdutoId = produtos[0].id;
+
+//         // CÓDIGO FICANDO ANINHADO
+//         fetch(`https://fakestoreapi.com/products/${primeiroProdutoId}`)
+//         .then(response => response.json())
+//         .then(destaqueProduto => {
+//             console.log("Produto em Destaque: ", destaqueProduto);
+//         });
+//     })
+//     .catch(erro => console.error('Erro ao buscar os produtos:', erro));
+// }
+
+// CÓDIGO DE BUSCA DA API REFATORADO
+async function carregarProdutos() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        if(!response.ok) {
+            throw new Error(`Erro HTTP: ${response.status}`);
+        }
+        const produtos = await response.json();
+        listaDeProdutos = produtos;
+
+        // Limpa os containers antes de renderizar
+        destaqueProduto.innerHTML = '';
+        catalogoProduto.innerHTML = '';
+
+        if(produtos.length > 0){
+
+            const primeiroProdutoId = produtos[0].id;
+            const responseDestaque = await fetch(`https://fakestoreapi.com/products/${primeiroProdutoId}`);
+            if(!response.ok){
+                throw new Error(`Erro HTTP ao buscar destaque: ${responseDestaque.status}`);
+            }
+            const produtoDestaque = await responseDestaque.json();
+
+            const cardDestaque = criarCardProduto(produtoDestaque);
+            destaqueProduto.appendChild(cardDestaque);
+        }
+
+        renderizarProdutos();
+        salvarProdutosNoStorage();
+    }
+    catch (erro) {
+        console.error("Ocorreu um erro na busca de produtos: ", erro);
+        catalogoProduto.innerHTML = '<p class="error">Não foi possível carregar os produtos!</p>'
+    }
 }
 
 formProduto.addEventListener('submit', function(event){
@@ -75,8 +121,17 @@ window.onload = function() {
     const produtosSalvos = localStorage.getItem('produtos');
     if (produtosSalvos) {
         listaDeProdutos = JSON.parse(produtosSalvos);
+
+        // Renderiza o primeiro produto a partir do storage
+        if(listaDeProdutos.length > 0) {
+            destaqueProduto.innerHTML = '';
+            const cardDestaque = criarCardProduto(listaDeProdutos[0]);
+            destaqueProduto.appendChild(cardDestaque);
+        }
+
+        // Renderiza o restante do catálogo
         renderizarProdutos();
     } else {
-        buscarProdutosDaApi(); // Só busca se tiver vazio
+        carregarProdutos(); // Só busca se tiver vazio
     }
 }
