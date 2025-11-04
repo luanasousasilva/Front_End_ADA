0f5e013ad36d2f3e87eb9b825e6621d8bbd51fb9
luana.silva <luana.silva@bitgov.com.br>
2025-11-03 22:15:33 -0300

    criação do header e do footer

--- Angular 1/minha-loja/src/app/components/header/header.component.ts
+++ Angular 1/minha-loja/src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CartStatusComponent } from '../cart-status/cart-status.component';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [CartStatusComponent],
+  imports: [CartStatusComponent,RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
